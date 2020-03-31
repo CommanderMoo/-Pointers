fdfcbce8a9616a63dc5a35e204e7d7964d58aa2f
RUSSELL PICKNEY <RUSPICKN@uat.edu>
2020-03-31 16:03:58 -0700

    added notes

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-// Pointers
+// -Pointers
 // this is in class (from recording in covid-19 03312020)
 #include <iostream>
 using namespace std;
@@ -32,3 +32,53 @@ int main()
 	// **********************************************************
 	return 0;
 }
+
+//notes
+/*
+{
+	//pointers
+#include <iostream>
+#include <ctime>
+#include <string>
+
+	int* pAPointer;	//declare a pointer
+	int* pScore = 0; // declare and initialize a pointer
+	int score = 1000;
+	pScore = &score; // assign pointer pScore address of variable score
+
+	cout << "Assigning &score to pScore \n";
+	cout << "&score is: " << &score << "\n"; //address of score variable
+	cout << "pScore is: " << pScore << ""; //address stored in pointer
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+	cout << "Adding 500 to score \n";
+	score += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is:" << *pScore << "\n\n";
+
+	cout << "Adding 500 to  *pScore \n";
+	*pScore += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+	cout << "Assigning &newScore to pScore \n";
+	int newScore = 5000;
+	pScore = &newScore;
+	cout << "&newScore is: " << &newScore << "\n";
+	cout << "pScore is: " << pScore << "\n";
+	cout << "newScore is: " << newScore << "\n";
+	cout << "*pScore is:" << *pScore << "\n\n";
+
+	cout << "Assigning &str to pStr \n";
+	string str = "score";
+	string* pStr = &str; //pointer to string object
+
+	cout << "str is: " << str << "\n";
+	cout << "*pStr is: " << *pStr << "\n";
+	cout << "(*pStr).size() is: " << (*pStr).size() << "\n";
+	cout << "pStr->size() is: " << pStr->size() << "\n";
+
+	return 0;
+}
+*/
